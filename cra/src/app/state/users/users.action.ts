ef16cde1d16e04277add3a8d5486fe6bdc2c59d2
Maxime Lassort <maximelassort@gmail.com>
2024-10-08 15:36:55 +0200

    factorisation signalStore

--- cra/src/app/state/users/users.action.ts
+++
@@ -1,4 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-import {User} from "../../core/models/user";
-
-export const loadUsers = createAction('[User/API] Load Users', props<{ users: User[] }>());
